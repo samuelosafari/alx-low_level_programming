3c5c84b9834160445ac99c2aadc46b163322df8d
Dream Coder <safarisamuel531@gmail.com>
2022-06-02 16:15:16 +0000

    a C program that uses prinf to output a sentence followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{printif("with proper grammar, but the outcome is a piece of art,\n");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
