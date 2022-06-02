52f8d55d3692cf0a60ba7ef7cc6c032efc1a03d2
Dream Coder <safarisamuel531@gmail.com>
2022-06-02 15:33:09 +0000

    a c program that prints out a sentence followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  print the strings in the puts function
+ * main - print the strings in the puts function
  * Description: prints "programming is like building a multilingual puzzle
  * Return: 0
  */
@@ -8,4 +8,4 @@ int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
 return (0);
-} 
+}
